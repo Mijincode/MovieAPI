3c25f68c7498c320c1ffa004060f140ad046497d
Mijin <mijindoig@hotmail.com>
2023-12-08 11:55:09 +1000

    Added comment to remove credentials

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,6 @@ module.exports = {
     host: "127.0.0.1",
     database: "movies",
     user: "root",
-    password: "Sue3129**",
+    password: "Sue3129**", // TODO: REMOVE THIS - NOT SAFE!!!
   },
 };
